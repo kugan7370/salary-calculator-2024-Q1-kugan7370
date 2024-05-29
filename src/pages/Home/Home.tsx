9d8aac1dcda918c67c782b8a39be9694314cfdc6
Kugan <kugapriyanj97@gmail.com>
2024-05-30 00:49:53 +0530

    initialization

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import "./Home.scss"
+
+const HomePage = () => {
+    return (
+        <div className="container">
+            <h1>Home P</h1>
+        </div>
+    )
+}
+
+export default HomePage
+
+
+
