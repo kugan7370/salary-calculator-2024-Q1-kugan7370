f6e281612e1df4f7357ea07467897677b81de9ba
Kugan <kugapriyanj97@gmail.com>
2024-05-31 01:05:32 +0530

    Persisted input data

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './styles/main.scss'
 import { Provider } from 'react-redux'
-import store from './store/store.ts'
+import store, { persistor } from './store/store.ts'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store}>
+
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
-    </Provider>
-  </React.StrictMode>,
+    </PersistGate>
+  </Provider>
+
 )
+
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,51 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import allowancesReducer from '../features/allowancesSlice';
 import deductionsReducer from '../features/deductionsSlice';
 import basicSalaryReducer from '../features/basicSalarySlice';
 
-const store = configureStore({
-  reducer: {
-    allowances: allowancesReducer,
-    deductions: deductionsReducer,
-    basicSalary: basicSalaryReducer
-  }
-});
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+
+
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+
+const rootReducer = combineReducers({
+
+
+  allowances: allowancesReducer,
+  deductions: deductionsReducer,
+  basicSalary: basicSalaryReducer
+
+})
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+
+export const persistor = persistStore(store)
+
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
