2022423fb16ac0e7c06031b5fb835b60426e3fff
Kugan <kugapriyanj97@gmail.com>
2024-05-31 01:29:49 +0530

    changed css file path

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './styles/main.scss'
+import './index.css'
 import { Provider } from 'react-redux'
 import store, { persistor } from './store/store.ts'
 import { PersistGate } from 'redux-persist/integration/react'
